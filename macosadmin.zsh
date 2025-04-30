207423d57f05649f3fa3041f67cd23763cc1ec05
kbrohia <kyle.brody@hopeisalive.net>
2025-04-30 11:26:26 -0500

    Update macosadmin.zsh
    Updated to remove user and create new admin if it already exists, shortened admin username

--- macosadmin.zsh
+++ macosadmin.zsh
@@ -1,12 +1,16 @@
-USERNAME="IT Admin"
+USERNAME="itadmin"  
 FULLNAME="IT Admin"
-PASSWORD="test!"
+PASSWORD="HIAmdm25!"
+
+echo "Checking if user '$USERNAME' exists..."
 
 if id "$USERNAME" &>/dev/null; then
-    echo "User '$USERNAME' already exists. Exiting."
-    exit 1
+    echo "User '$USERNAME' exists. Removing..."
+    sudo sysadminctl -deleteUser "$USERNAME" -secure
+    sleep 2
 fi
 
+echo "Creating admin user '$USERNAME'..."
 sudo sysadminctl -addUser "$USERNAME" -fullName "$FULLNAME" -password "$PASSWORD" -admin
 
 if id "$USERNAME" &>/dev/null; then
